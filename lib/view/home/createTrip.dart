cdf8b3b442022f8f01a9de451b6243320e0919e1
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2019-01-26 16:47:42 -0400

    🗺 Mejoras al seleccionar ubicacion

--- lib/view/home/createTrip.dart
+++ lib/view/home/createTrip.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:we_trip_out/view/components/dateTimePicker.dart';
 import 'package:we_trip_out/view/components/mapsLocationPicker.dart';
 import 'package:we_trip_out/view/components/tripButton.dart';
+import 'package:flutter/services.dart';
+import 'package:google_places_picker/google_places_picker.dart';
 
 class CreateTrip extends StatefulWidget {
   static const String routeName = '/trip/create';
@@ -11,25 +13,42 @@ class CreateTrip extends StatefulWidget {
 
 class _CreateTrip extends State<CreateTrip> {
   final _formKey = GlobalKey<FormState>();
-
+  String _placeName = 'Unknown';
   DateTime _fromDate = DateTime.now();
   TimeOfDay _fromTime = const TimeOfDay(hour: 7, minute: 28);
   DateTime _toDate = DateTime.now();
   TimeOfDay _toTime = const TimeOfDay(hour: 7, minute: 28);
   Map<String, double> _location = {};
 
+  _showPlacePicker() async {
+    String placeName;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    var place = await PluginGooglePlacePicker.showPlacePicker();
+    placeName = place.name;
+
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted)
+      return;
+
+    setState(() {
+      _placeName = placeName;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
       ),
       body: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            image: DecorationImage(
-                image: AssetImage("assets/edificios01.png"),
-                fit: BoxFit.contain,
-                alignment: Alignment.bottomCenter)),
+        // decoration: BoxDecoration(
+        //     color: Colors.white,
+        //     image: DecorationImage(
+        //         image: AssetImage("assets/edificios01.png"),
+        //         fit: BoxFit.contain,
+        //         alignment: Alignment.bottomCenter)),
         padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
@@ -37,7 +56,7 @@ class _CreateTrip extends State<CreateTrip> {
             children: <Widget>[
               RaisedButton(
                 onPressed: () {
-                  showMOdal();
+                  _showPlacePicker();
                 },
                 textColor: Colors.white,
                 child: Row(
@@ -49,7 +68,7 @@ class _CreateTrip extends State<CreateTrip> {
                 ),
                 color: Theme.of(context).accentColor,
               ),
-              Text('Ubicacion: $_location'),
+              Text('Ubicacion: $_placeName'),
               SizedBox(
                 height: 8.0,
               ),
